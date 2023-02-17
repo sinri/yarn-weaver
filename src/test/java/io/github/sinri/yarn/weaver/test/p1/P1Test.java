b95b5b8c5e30f8f4ee5cc2251ea1b8616f89fea4
Sinri Edogawa <ljni@leqee.com>
2023-02-17 16:09:07 +0800

    add p1 test

--- src/test/java/io/github/sinri/yarn/weaver/test/p1/P1Test.java
+++ src/test/java/io/github/sinri/yarn/weaver/test/p1/P1Test.java
@@ -1,10 +1,15 @@
 package io.github.sinri.yarn.weaver.test.p1;
 
 import io.github.sinri.keel.facade.Keel;
+import io.github.sinri.keel.helper.KeelHelpers;
 import io.github.sinri.keel.logger.event.KeelEventLogger;
 import io.github.sinri.keel.logger.event.center.KeelOutputEventLogCenter;
 import io.vertx.core.Future;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonArray;
+
+import java.util.Arrays;
+import java.util.Set;
 
 abstract public class P1Test {
     public static void init() {
@@ -12,8 +17,25 @@ abstract public class P1Test {
         Keel.initializeVertxStandalone(new VertxOptions());
     }
 
+    private static final Set<String> mixedStackPrefixSet = Set.of("io.vertx.", "java.", "io.netty.");
+
     protected KeelEventLogger getLogger() {
-        return KeelOutputEventLogCenter.getInstance().createLogger(this.getClass().getSimpleName());
+        return KeelOutputEventLogCenter.getInstance().createLogger(this.getClass().getSimpleName(), (eventLog) -> {
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+            if (stackTrace.length <= 5) {
+                eventLog.put("stack", null);
+            } else {
+                JsonArray array = new JsonArray();
+                StackTraceElement[] slice = Arrays.copyOfRange(stackTrace, 5, stackTrace.length);
+                KeelHelpers.jsonHelper().filterStackTrace(slice, mixedStackPrefixSet, (currentPrefix, ps) -> {
+                    array.add(currentPrefix + " × " + ps);
+                }, (stackTraceElement) -> {
+                    array.add(stackTraceElement.toString());
+                });
+                eventLog.put("stack", array);
+            }
+
+        });
     }
 
     abstract protected Future<Void> run();
@@ -26,6 +48,9 @@ abstract public class P1Test {
                 })
                 .onFailure(throwable -> {
                     getLogger().exception(throwable, "test failed");
+                })
+                .eventually(v -> {
+                    return Keel.getVertx().close();
                 });
     }
 }
